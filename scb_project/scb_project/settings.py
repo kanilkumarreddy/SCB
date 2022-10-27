2bfe8e78221d5bf37e15c19a57505fc63fb0a895
kanilkumarreddy <116055966+kanilkumarreddy@users.noreply.github.com>
2022-10-27 12:43:35 +0530

    change

--- scb_project/scb_project/settings.py
+++ scb_project/scb_project/settings.py
@@ -76,12 +76,8 @@ TEMPLATES = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
